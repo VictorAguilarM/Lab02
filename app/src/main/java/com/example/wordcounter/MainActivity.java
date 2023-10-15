6467f35037f6f3291d46655a67b7e243ab1b006a
victo <vam00022@red.ujaen.es>
2023-10-15 19:15:30 +0300

    Functionality

--- app/src/main/java/com/example/wordcounter/MainActivity.java
+++ app/src/main/java/com/example/wordcounter/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.wordcounter;
 
+import static java.lang.String.valueOf;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         Spinner spinner = (Spinner) findViewById(R.id.spCountOption);
 // Create an ArrayAdapter using the string array and a default spinner layout.
@@ -25,5 +35,24 @@ public class MainActivity extends AppCompatActivity {
 // Apply the adapter to the spinner.
         spinner.setAdapter(adapter);
 
+    }
+
+    public void btnCountClick(View view) {
+        EditText text = this.findViewById(R.id.edUserInput);
+        String userInput = text.getText().toString();
+        Log.i("UserText",userInput);
+
+        int count=0;
+        Spinner spin = this.findViewById(R.id.spCountOption);
+        if(spin.getSelectedItem().equals("Chars")){
+            count=WordCounter.getChars(userInput);
+        } else {
+            count=WordCounter.getWords(userInput);
+        }
+
+        Log.i("UserTextCount",String.valueOf(count));
+
+        TextView result = this.findViewById(R.id.tvResult);
+        result.setText(String.valueOf(count));
     }
 }
--- app/src/main/java/com/example/wordcounter/WordCounter.java
+++ app/src/main/java/com/example/wordcounter/WordCounter.java
@@ -1,4 +1,23 @@
 package com.example.wordcounter;
 
 public class WordCounter {
+    public static int getChars(String phrase){
+        return phrase.length();
+    }
+
+    public static int getWords(String phrase){
+        int count=0;
+        int bool=0;
+
+        for(int i=0; i<phrase.length(); i++){
+            if(phrase.charAt(i)==' '){
+                bool=0;
+            } else if(phrase.charAt(i)!=' ' && bool==0){
+                count++;
+                bool=1;
+            }
+        }
+
+        return count;
+    }
 }
